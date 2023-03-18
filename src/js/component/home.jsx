3c775e0428a599e7a7707174102cd48e3b329d63
Pedro López Murúa <pedro.lopez@prologic.cl>
2023-03-18 13:16:27 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+const[ color, setColor] = useState("");
+return (
+  <div className="box">
+  
+    <div className={color=='red'?"circle bg-danger glow": "circle bg-danger"}
+      onClick={()=>{setColor("red")}}></div>
+
+    <div className={color=='yellow'?"circle bg-warning glow": "circle bg-warning"}
+      onClick={()=>{setColor("yellow")}}></div>
+
+<div className={color=='green'?"circle bg-success glow": "circle bg-success"}
+      onClick={()=>{setColor("green")}}></div>
+
+
+
+  </div>
+
+
+);
+}
 
 export default Home;
